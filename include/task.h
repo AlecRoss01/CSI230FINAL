cb2dbc82aa748a36eafcbe8d0d511c2ea760297f
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2020-12-13 22:25:47 -0500

    Update task.h

--- include/task.h
+++ include/task.h
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-bool fileExists(string name); // done
-bool addTask(string name, string task); // done
-bool removeTask(string name, int taskNum); //done
-int getTaskAmount(string name); // done
-bool addPerson(string name); // done
-bool addGroup(string group); //done
-bool addPersonToGroup(string name, string group); //done
-void printTasks(string name); //done
-void printAssociations(string group); //done
+bool fileExists(string name);
+bool addTask(string name, string task);
+bool removeTask(string name, int taskNum);
+int getTaskAmount(string name);
+bool addPerson(string name);
+bool addGroup(string group);
+bool addPersonToGroup(string name, string group);
+void printTasks(string name);
+void printAssociations(string group);
